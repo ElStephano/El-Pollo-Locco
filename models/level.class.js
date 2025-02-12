3713860975069e3513fa7fa4d3b089001c851d34
ElStephano <165284686+ElStephano@users.noreply.github.com>
2025-02-12 16:32:44 +0100

    first commit

---
+++ models/level.class.js
@@ -0,0 +1,12 @@
+class Level {
+    enemies
+    clouds
+    backgroundObjects
+    level_end_x = 2165
+
+    constructor(enemies, clouds, backgroundObjects, level_end_x) {
+        this.enemies = enemies
+        this.clouds = clouds
+        this.backgroundObjects = backgroundObjects
+    }
+}
