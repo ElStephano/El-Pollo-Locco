97ea3f46d2f9fd91029181df5d1cb4364af35878
ElStephano <165284686+ElStephano@users.noreply.github.com>
2025-02-19 19:41:02 +0100

    statusBar

--- models/world.class.js
+++ models/world.class.js
@@ -1,11 +1,12 @@
 class World {
     character = new Character();
     level = level1
-    
     ctx
     canvas
     keyboard
     camera_x = 0
+    statusBar = new StatusBar()
+    bottleStatusBar = new BottleStatusBar()
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d')
@@ -26,7 +27,7 @@ class World {
             this.level.enemies.forEach((enemy) => {
                 if(this.character.isColliding(enemy)) {
                     this.character.hit()
-                    console.log(this.character.energy)
+                    this.statusBar.setPercentage(this.character.energy)
                 }
             })
     }, 1000)
@@ -37,8 +38,16 @@ class World {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         this.ctx.translate(this.camera_x, 0)
-
         this.addObjectsToMap(this.level.backgroundObjects)
+
+        this.ctx.translate(-this.camera_x, 0)
+        this.addToMap(this.statusBar)
+        this.ctx.translate(this.camera_x, 0)
+
+        this.ctx.translate(-this.camera_x, 0)
+        this.addToMap(this.bottleStatusBar)
+        this.ctx.translate(this.camera_x, 0)
+
         this.addObjectsToMap(this.level.clouds)
         this.addObjectsToMap(this.level.enemies)
         this.addToMap(this.character)
